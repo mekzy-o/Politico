391a8da6cc5652177be6a216635db2d99cdaf5c2
Emeka <mekzycodes@gmail.com>
2019-01-30 14:12:52 +0100

    [finishes 163588783] add get-party API endpoint

--- server/controllers/PartyController.js
+++ server/controllers/PartyController.js
@@ -17,26 +17,42 @@ class PartyController {
    */
   static createParty (req, res) {
       if(!req.body.name){
-      return res.status(400).send({
-      status: 400,
-      error: "name of party is required"
-    });
-      } else if (!req.body.hqAddress){
         return res.status(400).send({
-      status: 400,
-      error: "hqAddress of party is required"
+        status: 400,
+        error: "name of party is required"
+        });
+      } else if (!req.body.hqAddress){
+          return res.status(400).send({
+          status: 400,
+          error: "hqAddress of party is required"
       });
     }
+
     const party = {
-    id: partyDb.length + 1,
-    name: req.body.name,
-    hqAddress: req.body.hqAddress
+      id: partyDb.length + 1,
+      name: req.body.name,
+      hqAddress: req.body.hqAddress
     }
     partyDb.push(party);
     return res.status(201).send({
     status: 201,
     message: "party added successfully",
     party
+    });
+  }
+
+  /**
+   * @method getAllParties
+   * @description retrieves a list of all Parties
+   * @param {object} req - The Request Object
+   * @param {object} res - The Response Object
+   * @returns {object} JSON API Response
+   */
+  static getAllParties (req, res) {
+    res.status(200).send({
+    status: 200,
+    message: "parties retrieved successfully",
+    parties: partyDb
     });
   }
 }
