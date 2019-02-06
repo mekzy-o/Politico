e224f6aed26c5a64ec3e78aad7c805bfd99e92ce
Emeka <mekzycodes@gmail.com>
2019-02-06 15:29:08 +0100

    [chore #163712667] Refactor office controllers to use database

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import officeDb from '../datastore/officeDb';
-import partyDb from '../datastore/partyDb';
 import PartyController from '../controllers/PartyController';
 import OfficeController from '../controllers/OfficeController';
 
@@ -14,19 +12,19 @@ router.get('/', (req, res) => res.status(200).json({
 /* SETTING UP ENDPOINTS FOR PARTIES*/
 
 //create a party
-router.post('/api/v1/parties', PartyController.createParty);
+ router.post('/api/v1/parties', PartyController.createParty);
 
 //get all parties
-router.get('/api/v1/parties', PartyController.getAllParties);
+ router.get('/api/v1/parties', PartyController.getAllParties);
 
 //get a party using specific id
-router.get('/api/v1/parties/:id', PartyController.getParty);
+ router.get('/api/v1/parties/:id', PartyController.getParty);
 
 //deleting a party
-router.delete('/api/v1/parties/:id', PartyController.deleteParty);
+ router.delete('/api/v1/parties/:id', PartyController.deleteParty);
 
 //using patch method to Edit and update party name
-router.patch('/api/v1/parties/:id', PartyController.updatePartyName);
+ router.patch('/api/v1/parties/:id', PartyController.updatePartyName);
 
 /* SETTING UP ENDPOINTS FOR OFFICES*/
 
